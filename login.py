014740f45ce62f34b66c8fd3ed6abe36351b7b86
胡瑞祥 <45733030+HuRuixiang@users.noreply.github.com>
2019-04-30 14:32:01 +0800

    Delete login.py

--- login.py
+++
@@ -1,9 +0,0 @@
-import rquesrs
-import re
-import pickle
-from bs4 import BeautifulSoup
-import os
-
-with open("account.pickle") as file:
-  account = picke.load("account.pickle",file)
-print(account)
